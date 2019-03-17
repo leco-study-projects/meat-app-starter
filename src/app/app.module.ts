538a2c6771807d84ab87cfdddba17b5874487d86
leco <lcosta.sp.br@gmail.com>
2019-03-17 11:34:18 -0300

    create order component and shared component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule, LOCALE_ID} from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 import {ROUTES} from './app.routes';
 
@@ -18,6 +19,7 @@ import {ShoppingCartComponent} from './restaurant-detail/shopping-cart/shopping-
 import {MenuItemComponent} from './restaurant-detail/menu-item/menu-item.component';
 import {ReviewsComponent} from './restaurant-detail/reviews/reviews.component';
 import { OrderComponent } from './order/order.component';
+import { InputComponent } from './shared/input/input.component';
 
 @NgModule({
   declarations: [
@@ -32,12 +34,14 @@ import { OrderComponent } from './order/order.component';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent
+    OrderComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule,
+    FormsModule,
     RouterModule.forRoot(ROUTES, {enableTracing: true})
   ],
   providers: [RestaurantsService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
---
+++ src/app/shared/input/input.component.ts
@@ -0,0 +1,35 @@
+import {Component, Input, OnInit, ContentChild, AfterContentInit} from '@angular/core';
+
+import {NgModel} from '@angular/forms';
+
+@Component({
+  selector: 'mt-input-container',
+  templateUrl: './input.component.html'
+})
+export class InputComponent implements OnInit, AfterContentInit {
+
+  @Input() label: string;
+  @Input() errorMessage: string;
+
+  input: any;
+
+  @ContentChild(NgModel) model: NgModel
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  ngAfterContentInit() {
+    this.input = this.model;
+    if (this.input === undefined) {
+      throw  new Error('Esse componente precisa ser usado com a diretiva NgModel');
+    }
+  }
+
+  hasSuccess(): boolean {
+    return this.input.valid && (this.input.dirty || this.input.touched);
+  }
+
+}
